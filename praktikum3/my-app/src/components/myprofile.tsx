154370fa46e422b86add3ba179e555fb16977c2a
Yusfi Arif <arifyusfi1@gmail.com>
2024-03-01 21:08:19 +0700

    soal no 8

---
+++ praktikum3/my-app/src/components/myprofile.tsx
@@ -0,0 +1,52 @@
+// import { getImageUrlV2 } from '../utils/utils';
+
+// function MyAvatar({ person, size }) {
+//     return (
+//         <img
+//             className="avatar"
+//             src={getImageUrlV2(person, 'b')}
+//             alt={person.name}
+//             width={size}
+//             height={size}
+//         />
+//     );
+// }
+
+// export default function MyProfile() {
+//     return (
+//         <MyAvatar
+//             size={40}
+//             person={{
+//                 name: 'Gregorio Y. Zara',
+//                 imageId: '7vQD0fP'
+//             }}
+//         />
+//     );
+// }
+import { getImageUrlV2 } from '../utils/utils';
+
+function MyAvatar({ person, size }) {
+    const imageSize = size < 90 ? 's' : 'b'; 
+
+    return (
+        <img
+            className="avatar"
+            src={getImageUrlV2(person, imageSize)} // Use determined image size
+            alt={person.name}
+            width={size}
+            height={size}
+        />
+    );
+}
+
+export default function MyProfile() {
+    return (
+        <MyAvatar
+            size={90}
+            person={{
+                name: 'Gregorio Y. Zara',
+                imageId: '7vQD0fP'
+            }}
+        />
+    );
+}
