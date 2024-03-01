9fac4c734a17047b57d99cbbc9fce64b973e0991
Yusfi Arif <arifyusfi1@gmail.com>
2024-03-01 21:43:50 +0700

    soal no 8

--- praktikum3/my-app/src/components/myprofile.tsx
+++ praktikum3/my-app/src/components/myprofile.tsx
@@ -1,36 +1,38 @@
 // import { getImageUrlV2 } from '../utils/utils';
 
+// interface Person {
+//     name: string;
+//     imageId: string;
+// }
+
+// function MyAvatar({ person, size }: { person: Person; size: number }) {
 // function MyAvatar({ person, size }) {
 //     return (
 //         <img
 //             className="avatar"
-//             src={getImageUrlV2(person, 'b')}
-//             alt={person.name}
-//             width={size}
-//             height={size}
+@@ -28,23 +23,15 @@
 //         />
 //     );
 // }
 
-// export default function MyProfile() {
-//     return (
-//         <MyAvatar
-//             size={40}
-//             person={{
-//                 name: 'Gregorio Y. Zara',
-//                 imageId: '7vQD0fP'
-//             }}
-//         />
-//     );
-// }
+
 import { getImageUrlV2 } from '../utils/utils';
 
+interface Person {
+    name: string;
+    imageId: string;
+}
+
+function MyAvatar({ person, size }: { person: Person; size: number }) {
+    // Menentukan ukuran gambar berdasarkan nilai prop size
+    const imageSize = size < 90 ? 's' : 'b';
 function MyAvatar({ person, size }) {
     const imageSize = size < 90 ? 's' : 'b'; 
 
     return (
         <img
             className="avatar"
+            src={getImageUrlV2(person, imageSize)} // Menggunakan imageSize untuk menentukan ukuran gambar
             src={getImageUrlV2(person, imageSize)} // Use determined image size
             alt={person.name}
             width={size}
@@ -38,10 +40,10 @@ function MyAvatar({ person, size }) {
         />
     );
 }
-
 export default function MyProfile() {
     return (
         <MyAvatar
+            size={40}
             size={90}
             person={{
                 name: 'Gregorio Y. Zara',
