1821473a2bbb12eb7d8fa22157214266bcbdcc05
Yusfi Arif <arifyusfi1@gmail.com>
2024-03-01 21:42:03 +0700

    soal no 7

--- praktikum3/my-app/src/app/page.tsx
+++ praktikum3/my-app/src/app/page.tsx
@@ -1,6 +1,5 @@
 // import Image from "next/image";
 // import Profile from "../components/profile";
-
 // export default function Home() {
 //   return (
 // <div>
@@ -11,11 +10,8 @@
 //       <Profile></Profile>
 //     </div>
 // </div>
-
-
 //   );
 // }
-
 // import { Gallery } from "@/components/gallery";
 // export default function Home() {
 //   return (
@@ -25,52 +21,26 @@
 //     <Gallery />
 //   </div>
 // </div>
-
-
 //   );
 // }
-import Profile from "@/components/avatar";
+
 import { Gallery } from "@/components/gallery";
 import TodoList from "@/components/todolist";
 import Bio from "@/components/jsx";
 import MyGallery from "@/components/mygallery"
-import MyProfile from "@/components/myprofile"
-import MyProfileV2 from "@/components/myprofilev2"
-import Avatar from "@/components/avatar";
 
-function Card({ children }) {
-  return (
-    <div className="card">
-      {children}
-    </div>
-  );
-}
 export default function Home() {
   return (
-    <>
+    <section>
+      <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
       {/* <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
       <hr />
       <Gallery />
       <hr />
       <TodoList />
-      <hr />
-      <Bio /> */}
-      {/* <Card>
-        <Avatar
-          size={100}
-          person={{
-            name: 'Katsuko Saruhashi',
-            imageId: 'YfeOqp2'
-          }}
-        />
-      </Card> */}
-      {/* <MyGallery/> */}
-      {/* <MyProfile/> */}
-      <MyProfileV2/>
-      {/* <Profile /> */}
-    </>
-
+      <hr /> */}
+      {/* <Bio /> */}
+      <MyGallery/>
+    </section>
   );
 }
-
-
